bb65299efae38f490b5f6b0779ad7aaeaf6eabec
Stanley000 <zhenin.12@mail.ru>
2021-02-13 03:52:19 +0500

    Second commit

--- src/fixExitingEntity/Handler.java
+++ src/fixExitingEntity/Handler.java
@@ -1,6 +1,5 @@
 package fixExitingEntity;
 
-import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -10,12 +9,11 @@ import java.util.logging.Logger;
 public class Handler implements Listener {
 
     Logger log = Logger.getLogger("Minecraft");
-    private Entity Vehicle;
 
     @EventHandler
     public void onExit(PlayerQuitEvent event)
     {
-        if  (event.getPlayer().getVehicle() != Vehicle) {
+        if  (event.getPlayer().getVehicle() != null) {
             event.getPlayer().leaveVehicle();
             log.warning("[fixEntity] " + event.getPlayer().getDisplayName() + " the left the server while on entity!");
         }
